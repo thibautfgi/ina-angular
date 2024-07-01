6b7ca5907736c24b4aa3161018f363e966bf5354
ThibautFgi <thibaut.fgi@hotmail.com>
2024-07-01 14:54:22 +0200

    Etape 5 angular doc

--- ina-angular/src/app/app.component.ts
+++ ina-angular/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    HousingLocationComponent
+  ],
+  //templateUrl: './app.component.html',
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/picture1.jpg" alt="logo" height="200px" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'ina-angular';
+  title = 'ina-angular by tibo';
 }
