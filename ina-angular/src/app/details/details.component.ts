5d805a937472a938b5ca2f79a1ca0ea2e039a387
ThibautFgi <thibaut.fgi@hotmail.com>
2024-07-02 15:00:22 +0200

    angular finito, problemo appear

--- ina-angular/src/app/details/details.component.ts
+++ ina-angular/src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
     <article>
       <img
@@ -28,6 +29,23 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
           <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
         </ul>
       </section>
+      <section class="listing apply">
+        <h2 class="section-heading">Apply now to live here!</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+
+            <label for="first-name">First Name</label>
+            <input id="first-name" type="text" formControlName="firstName">
+
+            <label for="last-name">Last Name</label>
+            <input id="last-name" type="text" formControlName="lastName">
+
+            <label for="email">email</label>
+            <input id="email" type="email" formControlName="email">
+
+            <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
+      
     </article>
   `,
   styleUrl: './details.component.css'
@@ -36,8 +54,23 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '', // ?? = if the value of the left is null the value of right inside '' is use
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
--- ina-angular/src/app/home/home.component.ts
+++ ina-angular/src/app/home/home.component.ts
@@ -39,6 +39,8 @@ export class HomeComponent {
   housingService: HousingService = inject(HousingService);
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+    })
   }
 }
