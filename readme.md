426f3f55c0da097fde4e28ba5362a936fdcf38bc
ThibautFgi <thibaut.fgi@hotmail.com>
2024-07-04 16:23:34 +0200

    some bug fix

--- readme.md
+++ readme.md
@@ -1,13 +1,27 @@
 angular lurning 
 
-> go inside a repo for testing
+
+for second-app you need to execute =
+
+```
+ng add @fortawesome/angular-fontawesome
+```
+
+> go inside a repo for testing type =
 
 ```
 ng serve
 ```
 
-> you will need to use the local json api
+> you will need to use the local json api = 
 
 ```
 json-server --watch db.json
 ```
+
+order of creation of the app = 
+
+1- ina-angular
+2- second-app
+3- linkedapp
+
---
+++ second-app/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TaskService } from './services/task.service'; // Adjust the path as necessary
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, TasksComponent, HttpClientModule], // Include HttpClientModule here
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [TaskService] // Provide TaskService here
+})
+export class AppComponent {
+  constructor(private taskService: TaskService) {
+    this.taskService.getTasks().subscribe(tasks => {
+      console.log(tasks);
+    });
+  }
+}
