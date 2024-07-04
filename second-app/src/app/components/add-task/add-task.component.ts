68ca0badaa157a365a7de3a5e458bb92b907f2db
ThibautFgi <thibaut.fgi@hotmail.com>
2024-07-04 13:44:39 +0200

    app finito manque route

--- second-app/src/app/components/add-task/add-task.component.ts
+++ second-app/src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,14 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
@@ -17,8 +20,14 @@ export class AddTaskComponent {
   text!: string;
   day!: string;
   reminder: boolean= false; // default value
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value))
+   } // RUN quand un object ou un component est initialise
 
 
   onSubmit() {
--- second-app/src/app/services/ui.service.ts
+++ second-app/src/app/services/ui.service.ts
@@ -14,12 +14,18 @@ export class UiService {
   toggleAddTask(): void {
     this.showAddTask = !this.showAddTask; // l'inverse
     this.subject.next(this.showAddTask) // deviens la valeur a devenir ces a dire l'inverse
-
   }
+  //Subject est une classe de RxJS qui permet d'émettre des événements et de souscrire à ces événements.
+  //Ici, subject est utilisé pour émettre l'état de showAddTask chaque fois qu'il change.
+  // Utilise subject.next pour émettre la nouvelle valeur de showAddTask à tous les abonnés.
 
   onToggle(): Observable<any> { //complicado
     return this.subject.asObservable();
   }
+
+  // Renvoie un observable qui permet à d'autres composants de s'abonner aux changements de showAddTask.
+  //asObservable est utilisé pour encapsuler le Subject en tant qu'Observable, 
+  //empêchant ainsi les abonnés de modifier directement le Subject.
 
 
 }
