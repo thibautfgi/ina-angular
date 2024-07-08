ad577f21be3272f1851927a2acc7726e90ed148d
ThibautFgi <thibaut.fgi@hotmail.com>
2024-07-08 13:59:01 +0200

    deco terminado

--- linkedapp/src/app/components/footer/footer.component.ts
+++ linkedapp/src/app/components/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faQuestion = faQuestion;
 }
