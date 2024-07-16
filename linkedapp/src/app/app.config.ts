1ede8efd9e3b14ddeb10ea33d2adf1d224971fdd
ThibautFgi <thibaut.fgi@hotmail.com>
2024-07-16 12:05:23 +0200

    routeur

--- linkedapp/src/app/app.config.ts
+++ linkedapp/src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 // config les acces http ici
@@ -10,7 +8,6 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withHashLocation()),
     provideHttpClient(withFetch()),
     provideClientHydration()
   ]
